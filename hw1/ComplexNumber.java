136f498f3061364842b2cb8fec08cb4d523c5be9
Ernest Carter <Ernest Carter>
2016-12-03 13:31:23 -0600

    hw1. Needed to see if I could finish it.

--- hw1/ComplexNumber.java
+++ hw1/ComplexNumber.java
@@ -1,59 +1,115 @@
+/**
+ * Class that takes two floats as part of a complex number.
+ * another instaance of the class with it's own floats. 
+ * and adds, subtracts, multiplies, or divides the sets.
+ *
+ * @author Ernest Carter
+ */
 public class ComplexNumber
 {
-	
+	// first float of the complex number
 	private float a;
+	// second float of the complex number
 	private float b;
 	
+	/**
+	 * Assign the intance variable's of ComplexNumber
+	 * @param a first float of the complex number
+	 * @param b second float of the complex number
+	 */
 	public ComplexNumber(float a, float b) {
 		this.a = a;
 		this.b = b;
-	}
+	} // end constructor
 	
+	/**
+	 * Adds two complex numbers
+	 *
+	 * @param otherNumber second complex number
+	 * @return sum as a String
+	 */
 	public String add(ComplexNumber otherNumber) {
 		ComplexNumber newComplexNumber;
 		float newA = this.a + otherNumber.getA();
 		float newB = this.b + otherNumber.getB();
 		newComplexNumber = new ComplexNumber(newA, newB);
 		return toString(newComplexNumber);
-	}
+	} // end add method
 	
+	/**
+	 * Subtracts two complex numbers
+	 *
+	 * @param otherNumber second complex number
+	 * @return difference as a String
+	 */
 	public String subtract(ComplexNumber otherNumber) {
 		ComplexNumber newComplexNumber;
 		float newA = this.a - otherNumber.getA();
 		float newB = this.b - otherNumber.getB();
 		newComplexNumber = new ComplexNumber(newA, newB);
 		return toString(newComplexNumber);
-	}
+	} // end subtract method
 	
+	/**
+	 * Multplies two complex numbers
+	 *
+	 * @param otherNumber second complex number
+	 * @return product as a String
+	 */
 	public String multiply(ComplexNumber otherNumber) {
 		ComplexNumber newComplexNumber;
 		float newA = (this.a * otherNumber.getA()) - (this.b * otherNumber.getB());
 		float newB = (this.b * otherNumber.getA()) + (this.a * otherNumber.getB());
 		newComplexNumber = new ComplexNumber(newA, newB);
 		return toString(newComplexNumber);
-	}
+	} // end multiply method
 	
+	/**
+	 * Divides using two complex numbers.
+	 *
+	 * @param otherNumber second complex number
+	 * @return quotient as a String
+	 * @return "Cannot divide by zero" if otherNumber contains a zero
+	 */
 	public String divide(ComplexNumber otherNumber) {
 		ComplexNumber newComplexNumber;
-		float newA = ((this.a * otherNumber.getA()) + (this.b * otherNumber.getB())) / ((otherNumber.getA() * otherNumber.getA()) + (otherNumber.getB() * otherNumber.getB()));
-		float newB = ((this.b * otherNumber.getA()) - (this.a * otherNumber.getB())) / ((otherNumber.getA() * otherNumber.getA()) + (otherNumber.getB() * otherNumber.getB()));
-		newComplexNumber = new ComplexNumber(newA, newB);
-		return toString(newComplexNumber);
-	}
+		if ((otherNumber.getA() != 0) && (otherNumber.getB() != 0)) {
+			float newA = ((this.a * otherNumber.getA()) + (this.b * otherNumber.getB())) / ((otherNumber.getA() * otherNumber.getA()) + (otherNumber.getB() * otherNumber.getB()));
+			float newB = ((this.b * otherNumber.getA()) - (this.a * otherNumber.getB())) / ((otherNumber.getA() * otherNumber.getA()) + (otherNumber.getB() * otherNumber.getB()));
+			newComplexNumber = new ComplexNumber(newA, newB);
+			return toString(newComplexNumber);
+		}
+			return "Cannot divide by zero";
+	} // end method divide
 	
+	/**
+	 * Query's the first float of ComplexNumber
+	 * @return first float of ComplexNumber
+	 */
 	public float getA() {
 		return this.a;
-	}
+	} // end method getA
+	
+	/**
+	 * Query's the second float of ComplexNumber
+	 * @return second float of ComplexNumber
+	 */
 	public float getB() {
 		return this.b;
-	}
+	} // end method getB
 	
+	/**
+	* @Override
+	*/
 	public String toString(ComplexNumber stringNumber) {
+		String string;
 		if (stringNumber.getB() < 0) {
-		return stringNumber.getA() + " - " + stringNumber.getB() + "i";
+			string = stringNumber.getA() + " - " + (-1 * stringNumber.getB()) + "i";
+			return string;
 		}
 		else {
-			return stringNumber.getA() + " + " + stringNumber.getB() + "i";
+			string = stringNumber.getA() + " + " + stringNumber.getB() + "i";
+			return string;
 		}
-	}
-}
+	} // end toString method
+} // end class ComplexNumber
--- hw1/ComplexNumberTest.java
+++ hw1/ComplexNumberTest.java
@@ -2,6 +2,9 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+* Class that tests the class ComplexNumber
+*/
 public class ComplexNumberTest
 {
 	private ComplexNumber complexNumber;
@@ -10,6 +13,10 @@ public class ComplexNumberTest
 	private ComplexNumber complexNumberOneZero;
 	private ComplexNumber complexNumberNegative;
 	
+	/*
+	 * Sets up five instances of class ComplexNumber,
+	 * with complexNumber as the constant complex number.
+	 */
 	@Before
 	public void setUp() {
 		complexNumber = new ComplexNumber(3, 17);
@@ -17,37 +24,46 @@ public class ComplexNumberTest
 		complexNumberDecimal = new ComplexNumber(1.75f, 4.09f);
 		complexNumberOneZero = new ComplexNumber(5.99f, 0);
 		complexNumberNegative = new ComplexNumber(-6.66f, -17.77f);
-		
-	}
+	} // end method setup
 	
+	/**
+	 * Tests the add method of class ComplexNumber
+	 */
 	@Test
-	public void add() {
-		assertTrue( complexNumber.add(complexNumberTwoZeros) == "3 + 17i");
-		assertTrue( complexNumber.add(complexNumberDecimal) == "4.75 + 21.09");
-		assertTrue( complexNumber.add(complexNumberOneZero) == "8.99 + 17i");
-		assertTrue( complexNumber.add(complexNumberNegative) == "-3.66 - 0.77i");
-	}
+	public void testAdd() {
+		assertEquals( complexNumber.add(complexNumberTwoZeros), "3.0 + 17.0i");
+		assertEquals( complexNumber.add(complexNumberDecimal), "4.75 + 21.09i");
+		assertEquals( complexNumber.add(complexNumberOneZero), "8.99 + 17.0i");
+		assertEquals( complexNumber.add(complexNumberNegative), "-3.6599998 - 0.77000046i");
+	} // end method testAdd
 	
-	public void subtract() {
-		assertTrue( complexNumber.subtract(complexNumberTwoZeros) == "3 + 17i");
-		assertTrue( complexNumber.subtract(complexNumberDecimal) == "1.25 + 12.91i");
-		assertTrue( complexNumber.subtract(complexNumberOneZero) == "-2.99 + 17i");
-		assertTrue( complexNumber.subtract(complexNumberNegative) == "9.66 - 34.77i");
-	}
+	/**
+	 * Tests the subtract method of class ComplexNumber
+	 */
+	public void testSubtract() {
+		assertEquals( complexNumber.subtract(complexNumberTwoZeros), "3.0 + 17.0i");
+		assertEquals( complexNumber.subtract(complexNumberDecimal), "1.25 + 12.91i");
+		assertEquals( complexNumber.subtract(complexNumberOneZero), "-2.9899998 + 17.0i");
+		assertEquals( complexNumber.subtract(complexNumberNegative), "9.66 - 34.77i");
+	} // end method testSubtract
 	
-	public void multiply() {
-		assertTrue( complexNumber.multiply(complexNumberTwoZeros) == "0 + 0i");
-		assertTrue( complexNumber.multiply(complexNumberDecimal) == "-64.28 + 42.02");
-		assertTrue( complexNumber.multiply(complexNumberOneZero) == "17.97 + 101.83");
-		assertTrue( complexNumber.multiply(complexNumberNegative) == "282.11 - 166.53i");
-	}
+	/**
+	 * Tests the multiply method of class ComplexNumber
+	 */
+	public void testMultiply() {
+		assertEquals( complexNumber.multiply(complexNumberTwoZeros), "0.0 + 0.0i");
+		assertEquals( complexNumber.multiply(complexNumberDecimal), "-64.28 + 42.02");
+		assertEquals( complexNumber.multiply(complexNumberOneZero), "17.97 + 101.829994i");
+		assertEquals( complexNumber.multiply(complexNumberNegative), "282.11 - 166.53i");
+	} // end method testMultiply
 	
-	public void divide() {
-		assertTrue( complexNumber.add(complexNumberTwoZeros) == "3 + 17i");
-		assertTrue( complexNumber.add(complexNumberDecimal) == "3.77856 + 0.883248");
-		assertTrue( complexNumber.add(complexNumberOneZero) == "0.500835 + 2.83806i");
-		assertTrue( complexNumber.add(complexNumberNegative) == "-0.894320 - 0.166357i");
-	}
-	
-
-}
+	/**
+	 * Tests the divide method of class ComplexNumber
+	 */
+	public void testDivide() {
+		assertEquals( complexNumber.divide(complexNumberTwoZeros), "Cannot divide by zero");
+		assertEquals( complexNumber.divide(complexNumberDecimal), "3.778561 + 0.8832475i");
+		assertEquals( complexNumber.divide(complexNumberOneZero), "Cannot divide by zero");
+		assertEquals( complexNumber.divide(complexNumberNegative), "-0.89432965 - 0.16635728i");
+	} // end method testDivide
+} // end class ComplexNumberTest
